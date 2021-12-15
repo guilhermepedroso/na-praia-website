8ca22b4991f5ebeef9f13e32d3e1726e54103ec3
guilhermepedroso <guimeop@gmail.com>
2021-12-14 21:43:42 -0300

    fix: og type

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,26 +12,38 @@ export default function Home() {
     <>
       <Head>
         <title>Na Praia | A praia agora é digital!</title>
+        <meta name="title" content="Na Praia | A praia agora é digital!" />
         <meta
           name="description"
           content="Agora ficou fácil curtir um dia de sol no luga favorito do brasileiro. Vem curtir NaPraia!"
         />
-        <meta
-          name="keywords"
-          content="app, praia, comida, digital, pix, ambulante, vendedor, entrega, bebida, refrigerante, cerveja"
-        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://napraia.app/" />
         <meta
           property="og:title"
           content="Na Praia | A praia agora é digital!"
         />
-        <meta property="og:type" content="" />
-        <meta property="og:url" content="https://www.napraia.app/" />
         <meta
           property="og:description"
           content="Agora ficou fácil curtir um dia de sol no luga favorito do brasileiro. Vem curtir NaPraia!"
         />
-        <meta property="og:site_name" content="Na Praia " />
-        {/* <meta property="og:image" content="/hero.png"></meta> */}
+        <meta property="og:image" content="https://www.napraia.app/hero.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://napraia.app/" />
+        <meta
+          property="twitter:title"
+          content="Na Praia | A praia agora é digital!"
+        />
+        <meta
+          property="twitter:description"
+          content="Agora ficou fácil curtir um dia de sol no luga favorito do brasileiro. Vem curtir NaPraia!"
+        />
+        <meta
+          property="twitter:image"
+          content="https://www.napraia.app/hero.png"
+        />
       </Head>
       <Wrapper>
         <Hero />
