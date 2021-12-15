44e23e344f1d0291dd325ac2d0261408e7a82f75
guilhermepedroso <guimeop@gmail.com>
2021-12-14 21:33:51 -0300

    feat: added seo tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,28 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Na Praia | A praia na sua mão!</title>
+        <title>Na Praia | A praia agora é digital!</title>
+
+        <meta
+          name="description"
+          content="Agora ficou fácil curtir um dia de sol no luga favorito do brasileiro. Vem curtir NaPraia!"
+        />
+        <meta
+          name="keywords"
+          content="app, praia, comida, digital, pix, ambulante, vendedor, entrega, bebida, refrigerante, cerveja"
+        />
+        <meta
+          property="og:title"
+          content="Na Praia | A praia agora é digital!"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.napraia.app" />
+        <meta
+          property="og:description"
+          content="Agora ficou fácil curtir um dia de sol no luga favorito do brasileiro. Vem curtir NaPraia!"
+        />
+        <meta property="og:site_name" content="Na Praia " />
+        <meta property="og:image" content="/hero.png"></meta>
       </Head>
       <Wrapper>
         <Hero />
